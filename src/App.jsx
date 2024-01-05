8e15dac9602ffacfca2ebc27fe268bbb594c07c2
Ofori Emmanuel <emma@Oforis-MacBook-Pro.local>
2024-01-05 01:50:32 +0000

    refactored and responsive code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./About";
 import Apply from "./Apply";
 import Financial from "./financialaid";
 import Gallery from "./gallery";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
